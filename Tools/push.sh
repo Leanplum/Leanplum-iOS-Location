47fd8964f12590263f11af270de10149ee94170a
Mayank Sanganeria <mayank.sanganeria@leanplum.com>
2018-11-03 16:15:39 +0530

    setting up travis

---
+++ Tools/push.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+source ~/.rvm/scripts/rvm
+rvm use default
+pod trunk push Leanplum-iOS-Location.podspec --allow-warnings
+pod trunk push Leanplum-iOS-LocationAndBeacons.podspec --allow-warnings
